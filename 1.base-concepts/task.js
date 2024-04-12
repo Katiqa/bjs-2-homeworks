069186074133b803352e8813608d2bd8b96af0dc
Katiqa <katyushka.zykova.2015@mail.ru>
2024-04-12 14:24:28 +0300

    Homework1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = b ** 2 - 4 * a * c;
+
+	if (d === 0) {
+		let x = -b / (2 * a);
+		return [x];
+	} else if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		return [x1, x2];
+	} else {
+		return [];
+	}
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	const monthlyPercent = percent / 100 / 12;
+	const loanBody = amount - contribution;
+	const monthlyPayment = loanBody * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1));
+	const totalAmount = monthlyPayment * countMonths;
+
+	return Number(totalAmount.toFixed(2));
 }
