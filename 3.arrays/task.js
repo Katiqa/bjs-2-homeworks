a258871f73b4b2cd8f1a2f917967229493bb4cc0
Katiqa <katyushka.zykova.2015@mail.ru>
2024-04-17 20:06:12 +0300

    Homework 4

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((element, index) => element === arr2[index] && arr1.length === arr2.length);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filter = users.filter(user => user.gender === gender);
+    return filter.length ? filter.reduce((acc, user) => acc + user.age, 0) / filter.length : 0;
 }
